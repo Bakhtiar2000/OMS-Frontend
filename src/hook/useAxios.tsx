f5b48afc03a0b558df7e2afa17e378adb666d498
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2025-05-22 22:58:15 +0600

    added backend url

--- src/hook/useAxios.tsx
+++ src/hook/useAxios.tsx
@@ -1,8 +1,7 @@
 import axios from "axios";
-// import { getNewAccessToken } from "../utils/GetNewAccessToken";
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:5005/api/v1",
+  baseURL: "https://oms-backend-xiv3.onrender.com/api/v1",
 });
 
 axiosSecure.interceptors.request.use((config) => {
@@ -12,43 +11,5 @@ axiosSecure.interceptors.request.use((config) => {
   }
   return config;
 });
-
-// axiosSecure.interceptors.response.use(
-//   (response) => {
-//     return response;
-//   },
-//   async (error) => {
-//     const originalRequest = error.config;
-//     if (
-//       error.response.data.message.toLowerCase().includes("unauthorized") &&
-//       !originalRequest._retry
-//     ) {
-//       originalRequest._retry = true;
-
-//       try {
-//         const response = await getNewAccessToken();
-//         const accessToken = response?.data?.accessToken;
-
-//         if (accessToken) {
-//           localStorage.setItem("token", accessToken);
-//           originalRequest.headers["Authorization"] = accessToken;
-//           return axiosSecure(originalRequest);
-//         } else {
-//           // If no access token, logout
-//           localStorage.removeItem("token");
-//           localStorage.removeItem("user");
-//           window.location.href = "/";
-//           return Promise.reject(error);
-//         }
-//       } catch (refreshError) {
-//         localStorage.removeItem("token");
-//         localStorage.removeItem("user");
-//         window.location.href = "/login";
-//         return Promise.reject(refreshError);
-//       }
-//     }
-//     return Promise.reject(error);
-//   }
-// );
 
 export default axiosSecure;
